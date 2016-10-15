98e28220f792e86b48be7dc005e6faa3296d7a19
Dario L Garcia <dario.garcia@10pines.com>
2016-10-15 16:21:35 -0300

    Permitir crear reuniones

--- app/controllers/reuniones/list.js
+++ app/controllers/reuniones/list.js
@@ -6,6 +6,12 @@ export default Ember.Controller.extend(ReunionServiceInjected, NavigatorInjected
 
   anchoDeTabla: 's12',
 
+  reunionSeleccionada: Ember.computed('model.[]', 'indiceSeleccionado', function () {
+    var indiceSeleccionado = this.get('indiceSeleccionado');
+    var reuniones = this._reuniones();
+    return reuniones.objectAt(indiceSeleccionado);
+  }),
+
   actions: {
     verReunion(reunion){
       this._mostrarDetalleDe(reunion);
@@ -13,21 +19,46 @@ export default Ember.Controller.extend(ReunionServiceInjected, NavigatorInjected
     editarReunion(reunion){
       this.navigator().navigateToReunionesEdit(reunion.get('id'));
     },
+    crearReunion(){
+      this._guardarNuevaYRecargar();
+    },
     borrarReunion(reunion){
       this.reunionService().removeReunion(reunion)
         .then(()=> {
-          this.set('mostrandoDetalle', false);
-          this.set('anchoDeTabla', 's12');
-          // Actualizamos la lista
-          this.get('target.router').refresh();
+          this._ocultarDetalle();
+          this._recargarLista();
         });
     }
   },
 
   _mostrarDetalleDe(reunion){
-    this.set('reunionSeleccionada', reunion);
+    var indiceClickeado = this._reuniones().indexOf(reunion);
+    this.set('indiceSeleccionado', indiceClickeado);
+    this._mostrarDetalle();
+  },
+
+  _guardarNuevaYRecargar(){
+    this.reunionService().createReunion(Ember.Object.create())
+      .then(()=> {
+        this._recargarLista();
+      });
+  },
+
+  _recargarLista(){
+    this.get('target.router').refresh();
+  },
+
+  _ocultarDetalle(){
+    this.set('mostrandoDetalle', false);
+    this.set('anchoDeTabla', 's12');
+  },
+  _mostrarDetalle(){
     this.set('anchoDeTabla', 's4');
     this.set('mostrandoDetalle', true);
+  },
+
+  _reuniones(){
+    return this.get('model');
   }
 
 
