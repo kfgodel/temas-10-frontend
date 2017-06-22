cc04433221bbf19dabb1887d68c1674d404572b2
feche <fecheromero@gmail.com>
2017-06-22 16:19:00 -0300

    agregado color a los temas con votos propios

--- app/concepts/tema.js
+++ app/concepts/tema.js
@@ -7,10 +7,10 @@ export default Ember.Object.extend({
   }),
   colorVotos:Ember.computed('cantidadVotosPropios',function(){
       if(this.get('cantidadVotosPropios')>0){
-        return 'green'
+        return "#CEFFD4"
       }
       else{
-      return 'grey'
+      return 'white'
       }
   }),
   cantidadVotosPropios: Ember.computed('idsDeInteresados.[]', 'usuarioActual', function () {
