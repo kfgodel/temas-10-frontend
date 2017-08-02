779cfd5a43bb8f512d46b30a0b75b1d745f8beaf
Sandro <damilano.sandro@gmail.com>
2017-08-02 14:53:22 -0300

    Cambios menores en el txt

--- app/components/mostrador-de-minuta.js
+++ app/components/mostrador-de-minuta.js
@@ -10,31 +10,32 @@ export default Ember.Component.extend({
     let fecha = "Fecha: " + minuta.fecha + enter;
     let minuteador = "Minuteador: " + minuta.minuteador + enter;
 
-    let asistentes = "Vinieron:" + enter;
+    let asistentes = "Asistentes:" + enter;
     minuta.asistentes.forEach(function(asistente){
-      asistentes += tab + asistente.name + enter;
+      asistentes += tab + "* " + asistente.name + enter;
     });
 
     let temasPropuestos = "Temas propuestos:" + enter;
     minuta.temas.forEach(function(tema){
-      temasPropuestos += tab + tema.tema.titulo + enter;
+      temasPropuestos += tab + "- " + tema.tema.titulo + enter;
     });
 
     let temasTratados = "Detalle de los temas hablados:" + enter;
     let temaTratado = "";
-    minuta.temas.forEach(function(tema){
-      temaTratado = tab + tema.tema.titulo + enter;
+    minuta.temas.forEach(function(tema, index){
+      let indice = index + 1;
+      temaTratado = tab + indice + ". " + tema.tema.titulo + enter;
       temaTratado += tab + tab + "Conclusión:" + enter;
       let conclusionIdentada = tema.conclusion.replace(/\n/g, "\n" + tab + tab + tab);
       temaTratado += tab + tab + tab + conclusionIdentada + enter;
       temaTratado += tab + tab + "Action Items:" + enter;
 
       tema.actionItems.forEach(function(actionItem){
-        temaTratado += tab + tab + tab + actionItem.descripcion + enter;
-        temaTratado += tab + tab + tab + "Responsables:" + enter;
+        temaTratado += tab + tab + tab + "- " + actionItem.descripcion + enter;
+        temaTratado += tab + tab + tab + "  Responsables:" + enter;
 
         actionItem.responsables.forEach(function(responsable){
-          temaTratado += tab + tab + tab + tab + responsable.name + enter;
+          temaTratado += tab + tab + tab + tab + "* " + responsable.name + enter;
         });
       });
 
